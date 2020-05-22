0fdf00e67c5e5b7fbce0b3ef3dd22980c4752066
Endo_Hizumi <endohizumi0114@gmail.com>
2020-05-23 03:15:10 +0900

    update: Time can be set individually

--- mypomodoro/src/App.vue
+++ mypomodoro/src/App.vue
@@ -6,57 +6,111 @@
           <v-toolbar-title>my Pomodoro</v-toolbar-title>
           <v-spacer></v-spacer>
           <v-btn icon disable>
-            <v-icon >mdi-help-circle</v-icon>
+            <v-icon>mdi-help-circle</v-icon>
           </v-btn>
           <v-btn icon href="https://github.com/EndoHizumi/myPomodoro/tree/develop">
             <v-icon>mdi-github</v-icon>
           </v-btn>
         </v-app-bar>
       </div>
-      <v-card>
-        <timer
-          v-bind:name="name"
-          v-bind:startTime="time"
-          v-bind:state="state"
-          ref="timer"
-          v-on:finish="next"
-        ></timer>
-        <v-btn v-on:click="startTimer" v-if="state == 'stop' || state == 'finish'">
-          <v-icon x-large>mdi-play</v-icon>
-        </v-btn>
-        <v-btn v-on:click="startTimer" v-if="state == 'pause'">
-          <v-icon x-large>mdi-play</v-icon>
-        </v-btn>
-        <v-btn v-on:click="pauseTimer" v-if="state == 'process'">
-          <v-icon x-large>mdi-pause</v-icon>
-        </v-btn>
-        <v-btn v-on:click="stopTimer" v-if="state == 'pause' || state == 'finish'">
-          <v-icon x-large>mdi-stop</v-icon>
-        </v-btn>
-        <v-tabs v-model="tab" background-color="transparent" grow>
-          <v-tab v-for="item in pattern" :key="item">{{ item['name'] }}</v-tab>
-        </v-tabs>
+      <v-content>
+        <v-container fluid>
+          <timer
+            v-bind:name="name"
+            v-bind:startTime="time"
+            v-bind:state="state"
+            ref="timer"
+            v-on:finish="next"
+          ></timer>
 
-        <v-tabs-items v-model="tab">
-          <v-tab-item v-for="item in pattern" :key="item['id']">
-            <div v-for="element in item['data']" :key="element['id']">
-              {{element['name']}}:
-              <input
-                class="settime"
-                v-bind:disabled="state == 'process' || state == 'pause'"
-                type="number"
-                name="number"
-                v-bind:value="element.time"
-                v-on:input="onChange($event,element)"
-              />
-            </div>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-card>
-      <div class="group_volume">
-        <v-btn v-bind:disabled="state == 'process' || state == 'pause'" v-on:click='pattern[group]["data"].push({name:"task",time:"0"})'><v-icon>mdi-plus</v-icon></v-btn>
-        <v-btn v-bind:disabled="state == 'process' || state == 'pause'" v-on:click='pattern[group]["data"].pop()'><v-icon>mdi-minus</v-icon></v-btn>
-      </div>
+          <v-btn v-on:click="startTimer" v-if="state == 'stop' || state == 'finish'">
+            <v-icon x-large>mdi-play</v-icon>
+          </v-btn>
+          <v-btn v-on:click="startTimer" v-if="state == 'pause'">
+            <v-icon x-large>mdi-play</v-icon>
+          </v-btn>
+          <v-btn v-on:click="pauseTimer" v-if="state == 'process'">
+            <v-icon x-large>mdi-pause</v-icon>
+          </v-btn>
+          <v-btn v-on:click="stopTimer" v-if="state == 'pause' || state == 'finish'">
+            <v-icon x-large>mdi-stop</v-icon>
+          </v-btn>
+
+          <v-tabs v-model="tab" background-color="transparent" grow>
+            <v-tab v-for="item in pattern" :key="item">{{ item['name'] }}</v-tab>
+          </v-tabs>
+
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="item in pattern" :key="item['id']">
+              <div v-for="element in item['data']" :key="element['id']">
+                <v-layout column justify-center>
+                  <v-row>
+                    <v-col></v-col>
+                    <v-col>
+                      <v-text-field
+                        name="name"
+                        label="name"
+                        class="timername"
+                        v-model="element.name"
+                        v-on:input="onChange(element)"
+                        v-bind:disabled="state == 'process' || state == 'pause'"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col>
+                      <v-text-field
+                        class="timervalue"
+                        v-bind:disabled="state == 'process' || state == 'pause'"
+                        type="number"
+                        name="number"
+                        label="hour"
+                        v-model="element.hour"
+                        v-on:input="onChange(element)"
+                      />
+                    </v-col>
+                    <v-col>
+                      <v-text-field
+                        class="timervalue"
+                        v-bind:disabled="state == 'process' || state == 'pause'"
+                        type="number"
+                        name="number"
+                        label="minute"
+                        v-model="element.minute"
+                        v-on:input="onChange(element)"
+                      />
+                    </v-col>
+                    <v-col>
+                      <v-text-field
+                        class="timervalue"
+                        v-bind:disabled="state == 'process' || state == 'pause'"
+                        type="number"
+                        name="number"
+                        label="second"
+                        v-model="element.second"
+                        v-on:input="onChange(element)"
+                      />
+                    </v-col>
+                    <v-col></v-col>
+                  </v-row>
+                </v-layout>
+              </div>
+            </v-tab-item>
+          </v-tabs-items>
+          <div class="group_volume">
+            <v-btn
+              v-bind:disabled="state == 'process' || state == 'pause'"
+              v-on:click="pattern[group]['data'].push({name:'task',time:'0'})"
+            >
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+            <v-btn
+              v-bind:disabled="state == 'process' || state == 'pause'"
+              v-on:click="pattern[group]['data'].pop()"
+            >
+              <v-icon>mdi-minus</v-icon>
+            </v-btn>
+          </div>
+        </v-container>
+      </v-content>
     </v-app>
   </div>
 </template>
@@ -81,15 +135,11 @@ export default {
   methods: {
     startTimer: function() {
       this.state = "process";
-      this.name = this.pattern[this.group]["data"][this.position]["name"];
-      this.time = this.pattern[this.group]["data"][this.position]["time"];
       this.$refs.timer.startTimer();
     },
     stopTimer: function() {
       this.state = "stop";
       this.position = 0;
-      this.name = this.pattern[this.group]["data"][0]["name"];
-      this.time = this.pattern[this.group]["data"][0]["time"];
       this.$refs.timer.stopTimer();
     },
     pauseTimer: function() {
@@ -105,10 +155,19 @@ export default {
         this.position = 0;
       }
     },
-    onChange: function(event, element) {
-      element.time = event.target.value;
-      this.name = element.name
-      this.time = element.time;
+    onChange: function(element) {
+      
+      this.name = element.name;
+      this.time =
+        Math.abs(element.hour) * 3600 + Math.abs(element.minute) * 60 + Math.abs(parseInt(element.second));
+      if (Math.abs(element.second) > 59) {
+        element.minute = parseInt(Math.abs(element.minute)) + 1;
+        element.second = 0;
+      }
+      if (Math.abs(element.minute) > 59) {
+        element.minute = 0;
+        element.hour = parseInt(Math.abs(element.hour)) + 1;
+      }
     }
   },
   created: function() {
@@ -117,16 +176,24 @@ export default {
       data: [
         {
           name: "Work1",
-          time: "1800"
+          hour: "0",
+          minute: "30",
+          second: "0"
         },
         {
           name: "break1",
-          time: "300"
+          hour: "0",
+          minute: "5",
+          second: "0"
         }
       ]
     });
-    this.name = this.pattern[this.group]["data"][this.position]["name"];
-    this.time = this.pattern[this.group]["data"][this.position]["time"];
+    let postion_data = this.pattern[this.group]["data"][this.position];
+    this.name = postion_data["name"];
+    this.time =
+      postion_data.hour * 3600 +
+      postion_data.minute * 60 +
+      parseInt(postion_data.second);
   }
 };
 </script>
@@ -143,11 +210,6 @@ export default {
 #app > span > div > span {
   font-weight: bold;
   font-size: 1000%;
-}
-
-.settime {
-  margin-left: 50px;
-  width: 50px !important;
 }
 
 .group_volume {
